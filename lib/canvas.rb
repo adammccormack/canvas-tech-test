1b2cc8d1931dc3ff90a05972a8971d455d436339
Adam <adam.j.mccor@gmail.com>
2022-05-07 10:47:12 +0100

    branch 1 commit

--- lib/canvas.rb
+++ lib/canvas.rb
@@ -1,8 +1,23 @@
 
 class Canvas
 
-  def create(a, b)
-
+  def create(x, y)
+    
   end
+
+
+
+
+
+  def size
+    30
+  end
+
+  # def recursive_factorial(n)
+  #   # Base case
+  #   return 1 if n <= 1
+  #   # Recursive call
+  #   n * recursive_factorial(n-1)
+  # end
 
 end
--- spec/canvas_spec.rb
+++ spec/canvas_spec.rb
@@ -4,4 +4,14 @@ describe Canvas do
 
   it { is_expected.to respond_to(:create).with(2).argument }
 
+  describe '#create' do
+    it 'creates a new canvas with the two arguments given' do
+      canvas = Canvas.new
+
+      canvas.create(5,6)
+
+      expect(canvas.size).to eq(30)
+    end
+  end
+
 end
