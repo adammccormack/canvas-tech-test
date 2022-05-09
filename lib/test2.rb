6a0e355a3f0bcc701e021466e7cf5221e5e59a6d
Adam <adam.j.mccor@gmail.com>
2022-05-09 16:54:21 +0100

    removed commented function

--- lib/test2.rb
+++ lib/test2.rb
@@ -76,28 +76,6 @@ def reset
     end
   end
 
-  # def fill_down(row, col, grid = '@grid')
-#   grid = grid
-#   new_value = "N"
-#   @grid[row][col] = new_value
-#   while row >= 0
-#     row = row+1
-#     col = col
-#     pos_value = @grid[row][col]
-#     p "row_index: #{row}, col_index: #{col}"
-#     p "position_value: #{pos_value}"
-#   if pos_value != "O"
-#     break
-#   elsif row >= @grid.length-1
-#     @grid[row][col] = new_value
-#     break
-#   elsif pos_value == "O"
-#       @grid[row][col] = new_value
-#   else
-#     end
-#   end
-# end
-
   def move_right(row, col) 
     while col >= 0
       fill_up(row, col, @grid) unless col < 0
